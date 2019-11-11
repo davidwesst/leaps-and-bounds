94acd1b77757110d62d2c135f7b06529529b2a0e
David Wesst <david@cocobokostudios.com>
2019-11-11 00:32:02 -0600

    added player actionsn with physics bodies

--- player/player.gd
+++ player/player.gd
@@ -4,8 +4,21 @@ export var speed:int = 200
 
 var velocity: Vector2
 
+func try_action():
+	var bodies = $Range.get_overlapping_bodies()
+	bodies.erase(self)
+	
+	if bodies.size() > 0:
+		do_action(bodies[0])
+		
+func do_action(target: PhysicsBody2D):
+	print_debug(target)
+
 func get_input():
 	velocity = Vector2()
+	
+	if Input.is_action_just_pressed("player_action"):
+		try_action()
 	if Input.is_action_pressed("player_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("player_left"):
